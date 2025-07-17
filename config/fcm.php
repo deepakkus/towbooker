d0fa0e5ad337aff131c8ffc471c09cbddc46260f
deepakkus <deepak09.singh@gmail.com>
2025-07-17 14:56:38 +0530

    Code Uploaded

---
+++ config/fcm.php
@@ -0,0 +1,7 @@
+<?php
+
+// config/fcm.php
+
+return [
+    'token' => "AAAAJSC-HBE:APA91bEhh9zN5_pOb22zRvBXn4WT33FTC4B_OPDGWe4c1rn3c66nm9LwlMJAXvaYJALRGldrvzKSXudg4yXKCQr9n4QJKf3OtuNZWjApGohi1INmLnx72yKsO3NB5VOIReY4Y4nJxGIe",
+];
